56166fdd2fb4431504bd080b022ecf89ad33e666
bigsaigon333 <likepeppermint@gmail.com>
2021-12-14 19:32:28 +0900

    feat: notes 스키마와 리졸버를 구현한다

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import express from "express";
 import { ApolloServer, gql } from "apollo-server-express";
 
+const notes = [
+  { id: "1", content: "This is a note", author: "Adam Scott" },
+  { id: "2", content: "This is another note", author: "Harlow Everly" },
+  { id: "3", content: "Oh hey look, another note!", author: "Riley Harrison" },
+];
+
 const port = process.env.PORT || 4000;
 
 const app = express();
@@ -8,12 +14,39 @@ const app = express();
 const typeDefs = gql`
   type Query {
     hello: String
+    notes: [Note!]!
+    note(id: ID!): Note!
+  }
+
+  type Mutation {
+    newNote(content: String!): Note!
+  }
+
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
   }
 `;
 
 const resolvers = {
   Query: {
     hello: () => "Hello world!",
+    notes: () => notes,
+    note: (_: never, { id }: { id: string }) =>
+      notes.find((note) => note.id === id),
+  },
+  Mutation: {
+    newNote: (_: never, { content }: { content: string }) => {
+      const newValue = {
+        id: String(notes.length + 1),
+        content,
+        author: "bigsaigon333",
+      };
+      notes.push(newValue);
+
+      return newValue;
+    },
   },
 };
 
