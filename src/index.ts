49825c5df9d37916ae778268f9129a0a1834d254
bigsaigon333 <likepeppermint@gmail.com>
2021-12-14 15:50:25 +0900

    feat: ApolloServer를 실행한다

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,31 @@
 import express from "express";
+import { ApolloServer, gql } from "apollo-server-express";
 
 const port = process.env.PORT || 4000;
 
 const app = express();
-app.get("/", (req, res) => {
-  res.send("Hello, world!!!");
-});
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello world!",
+  },
+};
+
+(async () => {
+  const server = new ApolloServer({ typeDefs, resolvers });
+  await server.start();
+
+  server.applyMiddleware({ app, path: "/api" });
+
+  app.listen(port, () => {
+    console.log(
+      `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
+    );
+  });
+})();
