8a22b317389d527f9231d420285fc023db47bd67
bigsaigon333 <likepeppermint@gmail.com>
2021-12-16 23:44:59 +0900

    feat: JWT_SECRET 변수를 환경변수를 이용해 주입한다

--- src/constants.ts
+++ src/constants.ts
@@ -1,14 +1,12 @@
 import dotenv from "dotenv";
+import { throwError } from "./utils";
 
 dotenv.config();
 
-export const { DB_HOST, PORT } = (() => {
-  // eslint-disable-next-line @typescript-eslint/no-shadow
-  const { DB_HOST, PORT = 4000 } = process.env;
+export const DB_HOST =
+  process.env.DB_HOST ?? throwError("DB_HOST is undefined");
 
-  if (DB_HOST == null) {
-    throw new Error("DB_HOST is undefined");
-  }
+export const PORT = process.env.PORT ?? 4000;
 
-  return { DB_HOST, PORT };
-})();
+export const JWT_SECRET =
+  process.env.JWT_SECRET ?? throwError("JWT_SECRET is undefined");
