78b2fb321f430aa9574ae7a95af7d3bd7a0785ff
bigsaigon333 <likepeppermint@gmail.com>
2021-12-17 00:19:38 +0900

    feat: 사용자 인증 리졸버를 구현한다

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -1,10 +1,16 @@
-import type { Models } from "../models";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { AuthenticationError } from "apollo-server-express";
+import { JWT_SECRET } from "../constants";
+import type { Context } from "../index";
+
+const SALT_ROUNDS = 10;
 
 const Mutation = {
   newNote: async (
     _: never,
     { content }: { content: string },
-    { models }: { models: Models }
+    { models }: Context
   ) =>
     models.Note.create({
       content,
@@ -13,16 +19,53 @@ const Mutation = {
   updateNote: async (
     _: never,
     { id, content }: { id: string; content: string },
-    { models }: { models: Models }
+    { models }: Context
   ) => models.Note.findByIdAndUpdate(id, { $set: { content } }, { new: true }),
-  deleteNote: (
-    _: never,
-    { id }: { id: string },
-    { models }: { models: Models }
-  ) =>
+  deleteNote: (_: never, { id }: { id: string }, { models }: Context) =>
     models.Note.findByIdAndRemove(id)
       .then(() => true)
       .catch(() => false),
+  signUp: async (
+    _: never,
+    {
+      username,
+      email,
+      password,
+    }: { username: string; email: string; password: string },
+    { models }: Context
+  ) => {
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+
+    const user = await models.User.create({
+      username,
+      email: email.trim().toLowerCase(),
+      password: hashedPassword,
+    });
+
+    return jwt.sign({ id: user._id }, JWT_SECRET);
+  },
+  signIn: async (
+    _: never,
+    {
+      username,
+      email,
+      password,
+    }: (
+      | { username: string; email?: string }
+      | { username?: string; email: string }
+    ) & { password: string },
+    { models }: Context
+  ) => {
+    const user = await models.User.findOne({
+      $or: [{ username }, { email: email?.trim().toLowerCase() }],
+    });
+
+    if (user == null || !(await bcrypt.compare(password, user.password))) {
+      throw new AuthenticationError("[signIn] Error signing in");
+    }
+
+    return jwt.sign({ id: user._id }, JWT_SECRET);
+  },
 };
 
 export default Mutation;
