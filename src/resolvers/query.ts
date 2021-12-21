472f69ebdbd8f67b72dd480d5f04f10c1316f941
bigsaigon333 <likepeppermint@gmail.com>
2021-12-21 21:19:15 +0900

    feat: user 쿼리 리졸버를 추가한다

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -1,3 +1,5 @@
+import { AuthenticationError } from "apollo-server-express";
+import { throwError } from "../utils";
 import type { Context } from "../index";
 
 const Query = {
@@ -5,6 +7,25 @@ const Query = {
     models.Note.find(),
   note: async (parent: never, { id }: { id: string }, { models }: Context) =>
     models.Note.findById(id),
+  users: async (_: never, args: never, { models }: Context) =>
+    models.User.find({}),
+  user: async (
+    _: never,
+    { username }: { username: string },
+    { models }: Context
+  ) => {
+    return models.User.findOne({ username });
+  },
+  me: async (_: never, args: never, { models, user }: Context) => {
+    if (user == null) {
+      throw new AuthenticationError("[me] not signed in");
+    }
+
+    const me =
+      (await models.User.findById(user.id)) ?? throwError("[me] not found me");
+
+    return me;
+  },
 };
 
 export default Query;
